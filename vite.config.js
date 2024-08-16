0b91445df40476b0a6162ce0fdce8b3ccdb7a369
Mingx1994x <mggxmingx@gmail.com>
2024-08-16 17:09:17 +0800

    修改vite config路徑 每次都忘了...下次先到github 開repository好了...

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/',
+  base: '/HS_practice_Dashboard/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
